340038305a15b236f5a0e98e915d9bc34ef9b7d6
vijay ram <vijay.ramcse@gmail.com>
2014-12-16 13:20:25 -0500

    databasepolling
    data base polling and posting JSON to some endpoint

---
+++ src/main/java/com/database/polling/post/PollingMuleClient.java
@@ -0,0 +1,29 @@
+package com.database.polling.post;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+
+/**
+ *  * @author vijay
+ *
+ */
+
+
+
+
+public class PollingMuleClient{
+
+ 
+	
+	
+	public List<HashMap<Double, Double>> testMuleClientQueueAttrDetails(List<HashMap<Double,Double>> payload){
+		return payload;
+		
+	}
+	
+	
+
+
+}
